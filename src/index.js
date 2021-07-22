b149c4dc2f035bd86c2c69147d63319af927ed92
Elmer Lopez <meme_lopez10@hotmail.com>
2021-07-22 16:38:01 -0600

    submit event for form to add task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './style.css';
 import UI from './uUIi';
+import validateDescription from './validateDescription';
 
+// When content loads
 document.addEventListener('DOMContentLoaded', UI.addApp());
+
+// Event: when form is submitted
+document.querySelector('#addTaskForm').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const taskDescription = document.querySelector('#taskDesc').value.trim();
+  if (!validateDescription(taskDescription)) {
+    console.log('Enter valid description for to-do');
+  } else {
+    // Add task
+    UI.addTask(taskDescription);
+  }
+});
