2e51c202627ae17974b15efeb0e5adbd1126fd97
Elmer Lopez <meme_lopez10@hotmail.com>
2021-07-22 16:36:31 -0600

    Implement local storage use

--- src/store.js
+++ src/store.js
@@ -1,22 +1,38 @@
 // Store Class: Encapsulates de locacl storage
-import Task from './task';
-
 export default class Sotre {
+  // tasks
   static getTasks() {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
-      books = [];
+      tasks = [];
     } else {
-      books = JSON.parse(localStorage.getItem('tasks'));
+      tasks = JSON.parse(localStorage.getItem('tasks'));
     }
 
     return tasks;
   }
 
   static addTask(task) {
-    const tasks = Store.getTasks();
+    const tasks = this.getTasks();
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
+  // index - saves the total of tasks created ever
+  static getIndexTotal() {
+    let total;
+    if (localStorage.getItem('index') === null) {
+      total = 0;
+    } else {
+      total = JSON.parse(localStorage.getItem('index'));
+    }
+
+    return total;
+  }
+
+  static setsIndexTotalPlusOne() {
+    let total = this.getIndexTotal();
+    total += 1;
+    localStorage.setItem('index', JSON.stringify(total));
+  }
 }
