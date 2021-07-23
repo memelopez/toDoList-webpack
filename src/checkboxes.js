93720347b9a7cf2cea091e733eba0221df01d592
Elmer Lopez <meme_lopez10@hotmail.com>
2021-07-22 22:24:46 -0600

    Change parameters of taskCompleted method

--- src/checkboxes.js
+++ src/checkboxes.js
@@ -1,8 +1,21 @@
 //  Module: related to the status updates with completed checkboxes
+import UI from './uUIi';
 import Store from './store';
 
-const taskCompleted = (index) => {
-  console.log('Index recieved at checkboxes.js : ' + index + ' --bye.');
+const taskCompleted = (index, value) => {
+  // Gets list from local storage
+  const toDos = Store.getTasks();
+  toDos.forEach(task => {
+    if (task.index === index) { // Finds the specific item that has been completed
+      task.completed = value;
+    }
+  });
+
+  // Set items to storage 
+  Store.setTasks(toDos);
+
+  // Update list to UI
+  UI.addTasksUI(toDos);
 };
 
 export { taskCompleted };
