e89ec3a9fea8d57e8f12a38d4806bee767293ef2
Elmer Lopez <meme_lopez10@hotmail.com>
2021-07-22 18:11:30 -0600

    Add new JS file for status updates - completed: true/false

---
+++ src/checkboxes.js
@@ -0,0 +1,8 @@
+//  Module: related to the status updates with completed checkboxes
+import Store from './store';
+
+const taskCompleted = (index) => {
+  console.log('Index recieved at checkboxes.js : ' + index + ' --bye.');
+};
+
+export { taskCompleted };
